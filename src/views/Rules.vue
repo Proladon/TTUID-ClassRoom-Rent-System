0d3937cf8fce949e6790166df5c68f8a32521e8d
Proladon <proladon@gmail.com>
2021-11-06 17:30:57 +0800

    rules editor / rules page

--- src/views/Rules.vue
+++ src/views/Rules.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>dada</div>
+  <div class="rules markdown-body">
+    <div v-html="rulesContent"></div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, onMounted, ref } from '@vue/runtime-core'
+import sanitizeHtml from 'sanitize-html'
+import { useStore } from 'vuex'
 
-<style lang="postcss" scoped></style>
+const store = useStore()
+const config = computed(() => store.state.config)
+
+const rulesContent = ref<string>('')
+
+const cleanHtml = (source: string) => {
+  const res = sanitizeHtml(source, {
+    allowedAttributes: false,
+  })
+  rulesContent.value = res
+}
+
+onMounted(() => {
+  const rules = config.value.rules
+  // rulesContent.value = rules
+  cleanHtml(rules)
+})
+</script>
+
+<style lang="postcss" scoped>
+.rules {
+  @apply text-light-800 text-left max-w-[800px];
+}
+</style>
