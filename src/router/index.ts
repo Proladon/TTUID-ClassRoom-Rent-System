497d2d9de0928cc5800036d2f0f6dcdda97a373f
Proladon <proladon@gmail.com>
2022-03-07 00:07:17 +0800

    fix: router auth required guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Home from '@/views/Home.vue'
+import store from '@/store'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -40,6 +41,14 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.meta.requiresAuth) {
+    const {signin} = (store.state as any).authStore
+    if(!signin) return next({name: 'AdminLogin'})
+  }
+  next()
 })
 
 export default router
